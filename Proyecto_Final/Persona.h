b24e24e172f7d6bedff674461b40cca3f0189771
JennyCastillo <99575764+JennyCastillo@users.noreply.github.com>
2022-05-27 23:24:57 -0600

    Update Persona.h
    Personas

---
+++ Proyecto_Final/Persona.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class Persona
+{
+	
+protected: string nombres, apellidos, genero, correo_electronico, fecha_ingreso;
+		 int telefono = 0;
+
+protected:
+	Persona() {
+	}
+	Persona(string nom, string ape, string gen, int tel, string ce, string fi) {
+		nombres = nom;
+		apellidos = ape;
+		genero = gen;
+		telefono = tel;
+		correo_electronico = ce;
+		fecha_ingreso = fi;
+	}
+};
